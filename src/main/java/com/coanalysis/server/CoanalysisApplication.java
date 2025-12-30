8ee5ebad9d6f24b82137d33edff582abdc279122
fing9 <djajsl54@naver.com>
2025-12-30 15:20:32 +0900

    [Init] Project 기본 세팅 추가
    org.springframework.boot:spring-boot-starter-data-jpa
    org.springframework.boot:spring-boot-starter-web
    org.springdoc:springdoc-openapi-starter-webmvc-ui
    org.projectlombok:lombok
    
    종속성 추가

---
+++ src/main/java/com/coanalysis/server/CoanalysisApplication.java
@@ -0,0 +1,13 @@
+package com.coanalysis.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoanalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coanalysis/server/CoanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coanalysis.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoanalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
