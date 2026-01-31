385684a384363e3892d20b4fc1108e698c83b05b
fing9 <djajsl54@naver.com>
2026-01-31 23:36:32 +0900

    [Fix] Update SearchNewsController to use @PathVariable instead of @PathParam for ID retrieval

--- src/main/java/com/coanalysis/server/news/adapter/in/SearchNewsController.java
+++ src/main/java/com/coanalysis/server/news/adapter/in/SearchNewsController.java
@@ -8,14 +8,10 @@ import com.coanalysis.server.news.adapter.in.swagger.SearchNewsControllerSwagger
 import com.coanalysis.server.news.adapter.out.SearchNewsQuery;
 import com.coanalysis.server.news.application.domain.News;
 import com.coanalysis.server.news.application.port.in.SearchNewsUseCase;
-import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,7 +25,7 @@ public class SearchNewsController implements SearchNewsControllerSwagger {
 
     @Override
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<SearchNewsResponse>> searchById(@PathParam(value = "id") Long id) {
+    public ResponseEntity<ApiResponse<SearchNewsResponse>> searchById(@PathVariable("id") Long id) {
         if (ObjectUtils.isEmpty(id)) {
             throw new CustomException(ErrorCode.INVALID_INPUT_VALUE, "뉴스 ID가 필요합니다.");
         }
