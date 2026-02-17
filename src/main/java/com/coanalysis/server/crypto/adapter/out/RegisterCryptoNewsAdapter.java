d8ab139913eb3d229b9016f050182448400671c4
fing9 <djajsl54@naver.com>
2026-02-17 19:35:39 +0900

    feat: 뉴스 수집 서비스에서 암호화폐 저장 로직 개선
    - CollectedNews에서 키워드 매칭 로직을 제거함
    - RegisterCryptoNewsAdapter에서 CryptoNewsRepository를 사용하여 저장 로직 구현
    - save 및 saveAll 메서드에서 null 반환 대신 실제 저장 기능 추가

--- src/main/java/com/coanalysis/server/crypto/adapter/out/RegisterCryptoNewsAdapter.java
+++ src/main/java/com/coanalysis/server/crypto/adapter/out/RegisterCryptoNewsAdapter.java
@@ -6,19 +6,24 @@ import org.springframework.stereotype.Repository;
 
 import com.coanalysis.server.crypto.application.domain.CryptoNews;
 import com.coanalysis.server.crypto.application.port.out.RegisterCryptoNewsPort;
+import com.coanalysis.server.infrastructure.repository.CryptoNewsRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class RegisterCryptoNewsAdapter implements RegisterCryptoNewsPort {
+
+	private final CryptoNewsRepository repository;
+
 	@Override
 	public CryptoNews save(CryptoNews cryptoNews) {
-		return null;
+		return repository.save(cryptoNews);
 	}
 
 	@Override
 	public List<CryptoNews> saveAll(List<CryptoNews> cryptoNewsList) {
-		return List.of();
+		return repository.saveAll(cryptoNewsList);
 	}
+
 }
